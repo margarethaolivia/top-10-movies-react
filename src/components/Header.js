a36f2be0452dcf35b4c7af86dec6774ab5022758
Margaretha Olivia <margarethaolivia41@gmail.com>
2021-10-30 21:33:54 +0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <nav>
+      <ul class="nav-list">
+        <li class="nav-item strong">
+          <a href="/">Top10Movies</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
